4bb3348abc3bcd4b8435876494e202ad2bae9978
Jorge-DevOps <jorgedevops20@gmail.com>
2025-02-07 11:46:13 -0500

    first commit

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+import socket
+
+app = Flask(__name__)
+
+
+@app.route("/hello")
+def read_hello():
+    hostname = socket.gethostname()
+    return jsonify({"message": f"Hello from {hostname}"})
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000, host="0.0.0.0")
